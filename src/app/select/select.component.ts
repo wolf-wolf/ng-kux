71f5c8e9de17038ce3e27f17ef1f58e7ea80f2c6
Zhaoxin Liu <zhaoxinliu@ZhaoxindeMacBook-Pro.local>
2017-04-11 15:08:22 +0800

    submit

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs'
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.css']
 })
 export class SelectComponent implements OnInit {
-  private sub= new Subject();
   public options: any[] = [
     {
       name: "选择下面一项",
@@ -30,13 +28,7 @@ export class SelectComponent implements OnInit {
   ];
   public selected = -1;
   constructor() {
-    this.sub.subscribe((data)=>{
-        console.log(data)
-    })
-    this.sub.next(0);
-    setInterval(()=>{
-      this.sub.next(new Date())
-    },2000)
+
   }
 
   ngOnInit() {
